4f48a1a1d1ad137f9a57b76775ffc2739e1762d7
cristiansot <soluciongrafix@gmail.com>
2025-04-23 15:28:19 -0400

    Date.now

--- server/services/authManager.js
+++ server/services/authManager.js
@@ -31,7 +31,7 @@ export const getValidAccessToken = async () => {
     });
 
     const { access_token, refresh_token, expires_in } = response.data;
-    const expires_at = Date.now() + expires_in * 1000;
+    const expires_at = Date.now() + tokenData.expires_in * 1000;
 
     // Guardar los nuevos tokens
     saveTokens({
