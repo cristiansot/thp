8d449772c1b7757c186544a4d938b5cd53aa4355
cristiansot <soluciongrafix@gmail.com>
2025-05-03 13:15:17 -0400

    Email

--- server/services/mail.js
+++ server/services/mail.js
@@ -47,7 +47,9 @@ const sendEmailNotification = (property) => {
 
 export async function sendEmail({ to, subject, text }) {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
