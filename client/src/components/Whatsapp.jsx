c9f107ba2d6d3fd91a8e2064d973c1d5719a1a22
cristiansot <soluciongrafix@gmail.com>
2025-09-28 14:47:13 -0300

    Fixed

--- client/src/components/Whatsapp.jsx
+++ client/src/components/Whatsapp.jsx
@@ -21,7 +21,7 @@ import '../assets/css/whatsapp.css'
 const Whatsapp = () => {
   return (
     <div className="phone-call cbh-phone cbh-green cbh-show cbh-static" id="clbh_phone_div" 
-      style={{ position: 'absolute', bottom: '0px', right: '5px', zIndex: '2147483647' }}>
+      style={{ position: 'fixed', bottom: '0px', right: '5px', zIndex: '2147483647' }}>
       <a id="WhatsApp-button" href="whatsapp://send?text= Hola Total Home Propiedades, necesito de tus servicios&amp;phone=+56992992640" className="phoneJs" title="Enviar mensaje!">
         <div className="cbh-ph-circle"></div>
         <div className="cbh-ph-circle-fill"></div>
