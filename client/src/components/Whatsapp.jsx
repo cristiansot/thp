8953c64f63990535473364fa2bf302e70ac278de
cristiansot <soluciongrafix@gmail.com>
2025-09-28 14:58:46 -0300

    Back

--- client/src/components/Whatsapp.jsx
+++ client/src/components/Whatsapp.jsx
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import '../assets/css/whatsapp.css'
-
-// const Whatsapp = () => {
-//   return (
-//     <div className="phone-call cbh-phone cbh-green cbh-show cbh-static" id="clbh_phone_div" style={{ position: 'fixed', bottom: '0px', right: '5px', zIndex: '999' }}>
-//       <a id="WhatsApp-button" href="whatsapp://send?text= Hola Total Home Propiedades, necesito de tus servicios&amp;phone=+56992992640" className="phoneJs" title="Enviar mensaje!">
-//         <div className="cbh-ph-circle"></div>
-//         <div className="cbh-ph-circle-fill"></div>
-//         <div className="cbh-ph-img-circle1"></div>
-//       </a>
-//     </div>
-//   );
-// };
-
-// export default Whatsapp;
-
 import React from 'react';
 import '../assets/css/whatsapp.css'
 
@@ -38,7 +21,7 @@ export default Whatsapp;
 
 // const Whatsapp = () => {
 //   const whatsappButton = (
-//     <div
+//     <div className="phone-call cbh-phone cbh-green cbh-show cbh-static" id="clbh_phone_div" 
 //       style={{
 //         position: 'fixed',
 //         bottom: '0px',
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-//Local
+// Local
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 
