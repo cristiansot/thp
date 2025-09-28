4f4367c55b7280f6a662c2f13a63a2598a9f159b
cristiansot <soluciongrafix@gmail.com>
2025-09-28 13:40:27 -0300

    Z-index 999 whatsapp icon

--- client/src/components/Whatsapp.jsx
+++ client/src/components/Whatsapp.jsx
@@ -3,7 +3,7 @@ import '../assets/css/whatsapp.css'
 
 const Whatsapp = () => {
   return (
-    <div className="phone-call cbh-phone cbh-green cbh-show cbh-static" id="clbh_phone_div" style={{ position: 'fixed', bottom: '0px', right: '5px', zIndex: '80' }}>
+    <div className="phone-call cbh-phone cbh-green cbh-show cbh-static" id="clbh_phone_div" style={{ position: 'fixed', bottom: '0px', right: '5px', zIndex: '999' }}>
       <a id="WhatsApp-button" href="whatsapp://send?text= Hola Total Home Propiedades, necesito de tus servicios&amp;phone=+56992992640" className="phoneJs" title="Enviar mensaje!">
         <div className="cbh-ph-circle"></div>
         <div className="cbh-ph-circle-fill"></div>
