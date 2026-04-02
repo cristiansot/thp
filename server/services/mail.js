f0ed296faa08b614a9c239316a41d78a783fce8f
cristiansot <soluciongrafix@gmail.com>
2026-04-02 15:15:27 -0300

    Mail false

--- server/services/mail.js
+++ server/services/mail.js
@@ -29,7 +29,7 @@ const sendEmailNotification = (property) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
