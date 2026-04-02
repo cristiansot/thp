8d9a7bb52d177483b2c673f43e926b40a9093dc8
cristiansot <soluciongrafix@gmail.com>
2026-04-02 17:50:31 -0300

    mail

--- server/services/mail.js
+++ server/services/mail.js
@@ -4,13 +4,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // 🔥 Crear transporter UNA sola vez
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: Number(process.env.EMAIL_PORT),
+//   secure: Number(process.env.EMAIL_PORT) === 465, // true solo si puerto 465
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: Number(process.env.EMAIL_PORT) === 587, // true solo si puerto 465
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
+  },
+  requireTLS: true, // 🔥 IMPORTANTE
+  tls: {
+    ciphers: 'SSLv3',
   },
 });
 
