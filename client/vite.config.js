2cb05e886e9129dec58d92a25793f96dfac09a6e
cristiansot <soluciongrafix@gmail.com>
2026-04-02 22:00:25 -0300

    vite 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,24 +8,6 @@
 // });
 
 // Production
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig(({ mode }) => ({
-//   base: mode === 'production' ? '' : '/',
-//   plugins: [react()],
-//   server: {
-//     port: 10000,
-//     proxy: {
-//       '/api': {
-//         target: 'https://api.thp.cl/', 
-//         changeOrigin: true,
-//         secure: true,
-//       },
-//     },
-//   },
-// }));
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -36,18 +18,11 @@ export default defineConfig(({ mode }) => ({
     port: 10000,
     proxy: {
       '/api': {
-        target: 'https://api.thp.cl',  // SIN barra final
+        target: 'https://api.thp.cl/', 
         changeOrigin: true,
         secure: true,
       },
     },
   },
-  // Define variables de entorno por defecto
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
-      mode === 'production' 
-        ? 'https://api.thp.cl' 
-        : 'http://localhost:10000'
-    )
-  }
 }));
+
