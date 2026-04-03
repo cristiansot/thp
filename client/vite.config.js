68b9b43bbcd4b69e36092846902aa10bb5775a55
cristiansot <soluciongrafix@gmail.com>
2026-04-02 21:05:27 -0300

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
     port: 10000,
     proxy: {
       '/api': {
-        target: 'https://api.thp.cl/', 
+        target: 'https://api.thp.cl', 
         changeOrigin: true,
         secure: true,
       },
