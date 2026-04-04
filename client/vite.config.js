f5ec15b8649b1f55ed2c0ad9306af3db64a9497c
cristiansot <soluciongrafix@gmail.com>
2026-04-04 13:37:36 -0300

    Vite 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,8 @@
 // });
 
 // Production
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // export default defineConfig(({ mode }) => ({
 //   base: mode === 'production' ? '' : '/',
