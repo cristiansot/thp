d0ac57b5cde781da0a335aa587a5981574ab24da
cristiansot <soluciongrafix@gmail.com>
2026-07-06 12:23:36 -0400

    Se vende Ñuñoa

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,15 +4,24 @@ import '../assets/css/carousel.css';
 import image1 from '../assets/img/carousel/image1.webp';
 import image2 from '../assets/img/carousel/image2.webp';
 import image3 from '../assets/img/carousel/image3.webp';
+import image4 from '../assets/img/carousel/image4.webp';
 
 const slides = [
+   {
+    subtitle: 'Ñuñoa',
+    title: 'Se Vende Departamento',
+    price: 'UF 5290',
+    description:
+      'A pasos de Plaza Ñuñoa',
+    imageUrl: image1,
+  },
   {
     subtitle: 'Ñuñoa',
     title: 'Se Arrienda Departamento',
     price: '$860.000',
     description:
       'A pasos del Metro Simón Bolivar',
-    imageUrl: image1,
+    imageUrl: image2,
   },
    {
     subtitle: 'Ñuñoa',
@@ -20,7 +29,7 @@ const slides = [
     price: 'UF 7.420',
     description:
       'A pasos del Metro Chile España',
-    imageUrl: image2,
+    imageUrl: image3,
   },
     {
     subtitle: 'Ñuñoa',
@@ -28,7 +37,7 @@ const slides = [
     price: '$800.000',
     description:
       'Cerca de Metro Monseñor Eyzaguirre',
-    imageUrl: image3,
+    imageUrl: image4,
   },
 ];
 
