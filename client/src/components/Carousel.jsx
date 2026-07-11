88166f0822728ba2c5cfa0a1414b74bffc875b3e
cristiansot <soluciongrafix@gmail.com>
2026-07-11 15:31:03 -0400

    Despublicacion depto

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/css/carousel.css';
 
-import image1 from '../assets/img/carousel/image1.webp';
 import image2 from '../assets/img/carousel/image2.webp';
 import image3 from '../assets/img/carousel/image3.webp';
 import image4 from '../assets/img/carousel/image4.webp';
 
 const slides = [
-   {
-    subtitle: 'Ñuñoa',
-    title: 'Se Vende Departamento',
-    price: 'UF 5.290',
-    description:
-      'A pasos de Plaza Ñuñoa',
-    imageUrl: image1,
-  },
+
   {
     subtitle: 'Ñuñoa',
     title: 'Se Arrienda Departamento',
